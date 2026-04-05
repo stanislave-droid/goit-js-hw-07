bb72724680b1807ecb106b5a4b39aa1624d33547
Stanislave <stasdroner@gmail.com>
2026-04-05 20:37:40 +0300

    Add task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", logIn);
+
+function logIn(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email;
+  const password = form.elements.password;
+
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    alert("All form fields must be filled in");
+  } else {
+    console.log({
+      [email.name]: email.value.trim(),
+      [password.name]: password.value.trim(),
+    });
+  }
+
+  event.target.reset();
+}
